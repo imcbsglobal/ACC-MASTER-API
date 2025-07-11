d933f7647ba3ec3f07c396a0c4a6c622db3f8453
Krishna Kumar <149694348+KRISHNAKUMARPS2002@users.noreply.github.com>
2025-07-11 13:02:29 +0530

    views edited to get balance

--- syncdata/serializers.py
+++ syncdata/serializers.py
@@ -3,24 +3,39 @@ from .models import IMC1Record, IMC2Record, SysmacRecord, DQRecord
 
 
 class IMC1Serializer(serializers.ModelSerializer):
+    balance = serializers.SerializerMethodField()
     class Meta:
         model = IMC1Record
         fields = '__all__'
 
+    def get_balance(self, obj):
+        return (obj.opening_balance or 0) + (obj.debit or 0) - (obj.credit or 0)
+
 
 class IMC2Serializer(serializers.ModelSerializer):
+    balance = serializers.SerializerMethodField()
     class Meta:
         model = IMC2Record
         fields = '__all__'
 
+    def get_balance(self, obj):
+        return (obj.opening_balance or 0) + (obj.debit or 0) - (obj.credit or 0)
 
 class SysmacSerializer(serializers.ModelSerializer):
+    balance = serializers.SerializerMethodField()
     class Meta:
         model = SysmacRecord
         fields = '__all__'
 
+    def get_balance(self, obj):
+        return (obj.opening_balance or 0) + (obj.debit or 0) - (obj.credit or 0)
+
 
 class DQSerializer(serializers.ModelSerializer):
+    balance = serializers.SerializerMethodField()
     class Meta:
         model = DQRecord
         fields = '__all__'
+
+    def get_balance(self, obj):
+        return (obj.opening_balance or 0) + (obj.debit or 0) - (obj.credit or 0)
